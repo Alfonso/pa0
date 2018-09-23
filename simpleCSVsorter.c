fe503cbdc1d9f269189d9303df4fada1f600106a
Amulya S. Mummaneni <asm229@cpp.cs.rutgers.edu>
2018-09-23 19:22:14 -0400

    trying 2D array

--- simpleCSVsorter.c
+++ simpleCSVsorter.c
@@ -4,7 +4,7 @@
 #include <mergesort.h>
 #include <time.h>
 
-
+/*
 typedef struct _row{
 	// make a field for every column (?)
 	char* color;
@@ -37,35 +37,78 @@ typedef struct _row{
 	int movie_facebook_likes;
 	struct row* next;
 } row;
+*/
 
-row* makeStruct(file){
+node* makeStruct(file){
+
+	
+	// remaining problem: how to deal with different types?!?
+		// some thoughts: atoi and .isdigit()
+	
+
+	// scan csv 
+	// scan first row differently (bc names)
+		// go column by column, incrementing counter as you go (use temp)
+		// if input matches column, save this as counter value (counter = temp)
+		// continue until end to save number of columns
+	
+	// need number of rows for 2D array
+		// traverse csv for numRows
+		// rewind file
+		// traverse and populate array
+	
+	// at this point, array is set up
+		// traverse column of interest
+			// if NULL, add to separate list
+				// keep this in original order
+				// must append to front of sorted list
+				// save references to head and tail of this list!! 
+			// make node - 2 fields
+				// array[i][j]
+				// reference to current row
+	
+	// sort list
+	
+	// null list tail -> next = head of sorted list
+	
+	// return head of null list
+	
+
+
+	/*
+ 
 	// scan csv file
 	row* head = (row*)malloc(sizeof(row));
 	row* realHead = head;
 	// do one scan to process column names
 
+le (fscanf(file, "%s,%s,%d,%d,%d,%d,%s,%d,%d,%s,%s,%s,%d,%d,%s,%d,%s,%s,%d,%s,%s,%s,%d,%d,%d,%d,%d,%d\n",
+                 &color, &director_name, &num_critic_for_reviews, &duration, &director_facebook_likes, &actor_3_facebook_likes, &actor_2_name, &actor_1_facebook_likes, &gross, &genres, &actor_1_name, &movie_title, &num_voted_users, &cast_total_facebook_likes, &actor_3_name, &facenumber_in_poster, &plot_keywords, &movie_imdb_link, &num_user_for_reviews, &language, &country, &content_rating, &budget, &title_year, &actor_2_facebook_likes, &imdb_score, &aspect_ratio, &movie_facebook_likes)==28){
+	continue;
+	// what would i need to do lol	
+}
 	// rest of scanning
-	while (fscanf(file, "%s,%s,%d,%d,%d,%d,%s,%d,%d,%s,%s,%s,%d,%d,%s,%d,%s,%s,%d,%s,%s,%s,%d,%d,%d,%d,%d,%d",
+	while (fscanf(file, "%s,%s,%d,%d,%d,%d,%s,%d,%d,%s,%s,%s,%d,%d,%s,%d,%s,%s,%d,%s,%s,%s,%d,%d,%d,%d,%d,%d\n",
 		 &color, &director_name, &num_critic_for_reviews, &duration, &director_facebook_likes, &actor_3_facebook_likes, &actor_2_name, &actor_1_facebook_likes, &gross, &genres, &actor_1_name, &movie_title, &num_voted_users, &cast_total_facebook_likes, &actor_3_name, &facenumber_in_poster, &plot_keywords, &movie_imdb_link, &num_user_for_reviews, &language, &country, &content_rating, &budget, &title_year, &actor_2_facebook_likes, &imdb_score, &aspect_ratio, &movie_facebook_likes)==28){		
 		// make each row a struct ptr
 		row* temp = (row*)malloc(sizeof(row));
 		*temp = {color, director_name, num_critic_for_reviews, duration, director_facebook_likes, actor_3_facebook_likes, actor_2_names, actor_1_facebook_likes, gross, genres, actor_1_name, movie_title, num_voted_users, cast_total_facebook_likes, actor_3_name, facenumber_in_poster, plot_keywords, movie_imdb_link, num_user_for_reviews, language, country, content_rating, budget, title_year, actor_2_facebook_likes, imdb_score, aspect_ratio, movie_facebook_likes, head};
-		head = temp;
-
+		
+		//head = temp;
+		
 		// REMEMBER to add attribute node and point it to row node
 	}
 	// create a LL + return head
 	return realHead;	
 	
+	*/
 }
 
 void sortAndWrite(FILE* file, column){
 	node* csvStruct = makeStruct(file); // scan csv and create struct LL to represent it
-	// take nodes with null data and put in on list, keeping order that we read from
-	// reattach these to the front after sorting
-	 mergesort(&csvStruct); // sort struct LL
-	// reattach nodes with null data to beginning of the sorted list
-	
+		// take nodes with null data and put in on list, keeping order that we read from
+		// reattach these to the front after sorting
+		// sort struct LL
 	// convert to file
 	FILE* result = fopen("result.csv", "w+");
 	while(csvStruct != NULL){
